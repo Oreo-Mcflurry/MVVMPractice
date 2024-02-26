3d685c45191f7b0079b63e336727ecc4a16491fa
유인호 <yuiop1029@naver.com>
2024-02-26 16:05:36 +0900

    Initial Commit

---
+++ MVVMPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMPractice
+//
+//  Created by A_Mcflurry on 2/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
